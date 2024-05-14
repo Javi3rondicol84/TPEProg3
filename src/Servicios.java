ae2f5d74179ebf3b07bfb387e35ba07619bf3efd
Luciana444 <florteria@hotmail.com>
2024-05-14 19:13:05 -0300

    parte 1 terminada

--- src/Servicios.java
+++ src/Servicios.java
@@ -66,10 +66,16 @@ public class Servicios {
 		if(actual == null){
 			return lista;
 		}
-			buscarMinMax(actual.getLeft(),prioridadInferior,prioridadSuperior,lista);
-			buscarMinMax(actual.getRight(),prioridadInferior,prioridadSuperior,lista);
 			if(actual.getValue().getPrioridad() > prioridadInferior && actual.getValue().getPrioridad() < prioridadSuperior){
 				lista.add(actual.getValue());
+				buscarMinMax(actual.getLeft(),prioridadInferior,prioridadSuperior,lista);
+				buscarMinMax(actual.getRight(),prioridadInferior,prioridadSuperior,lista);
+			}
+			if(actual.getValue().getPrioridad() >= prioridadSuperior){
+				buscarMinMax(actual.getLeft(),prioridadInferior,prioridadSuperior,lista);
+			}
+			if(actual.getValue().getPrioridad() <= prioridadInferior){
+				buscarMinMax(actual.getRight(),prioridadInferior,prioridadSuperior,lista);
 			}
 			return lista;
 	}
@@ -78,6 +84,9 @@ public class Servicios {
 	
 	/*
      * Expresar la complejidad temporal del servicio 1.
+     * La complejidad computacional de este servicio es O(1), porque utilizamos la estructura de hashMap que contiene indices asociados 
+     * como valor clave, en este caso un String (id de la tarea) y un puntero al objeto Tarea con todos sus atributos, de esta forma acceder 
+     * a la tarea solicitada por el usuario a traves del id resulta en un tiempo constante. 
      */
 	public Tarea servicio1(String ID) {
 		return this.hashMap.get(ID);
@@ -85,6 +94,10 @@ public class Servicios {
     
     /*
      * Expresar la complejidad temporal del servicio 2.
+     * La complejidad computacional de este servicio tambien es O(1), porque utilizamos un metodo que nos rellena dos listas
+     * (una con las tareas criticas y la otra con las tareas no criticas), asi cuando el ususario nos pasa true o false, directamente
+     * le retornamos la lista previamente armada en el contructor (este llenado de lista se hace una unica vez en el constructor
+     * mientras que al servicio lo solicitan muchas veces)
      */
 	public List<Tarea> servicio2(boolean esCritica) {
 		if(esCritica){
@@ -95,6 +108,10 @@ public class Servicios {
 
     /*
      * Expresar la complejidad temporal del servicio 3.
+     * La complejidad computacional de este servicio en el peor de los casos es O(n), debido a que los valores maximo y minimo que nos
+     * pasa el usuario podrian encontrarse entre la hoja mas izquierda y la hoja mas derecha del arbol, pero en promedio este servicio 
+     * baja su complejidad a O(log n), porque en muchas ocaciones no es necesario recorrer todo el arbol, ya que este corta antes cuando 
+     * encuentra un valor contemplado dentro del rango.
      */
 	public List<Tarea> servicio3(int prioridadInferior, int prioridadSuperior) {
 		if(this.arbol.getRaiz() == null){
